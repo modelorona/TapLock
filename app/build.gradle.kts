1fd8391f5b591e03eb44d9d4ca3e8e6a0996c822
modelorona <angel.hristozov@gmail.com>
2026-03-14 12:21:01 +0000

    bump version for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ah.taplock"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.6"
+        versionCode = 11
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
